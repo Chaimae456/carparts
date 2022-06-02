4fba815dc548cfbf3d9306f1aecb49b8100000e3
chaimae <solicode.chaimae@gmail.com>
2022-06-02 16:56:19 +0100

    done

---
+++ carparts/app/src/main/java/com/example/carpartsalpha/SplashScreen.kt
@@ -0,0 +1,54 @@
+package com.example.carpartsalpha
+
+import android.content.Intent
+import android.graphics.Typeface
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_splash_screen)
+        @Suppress("DEPRECATION")
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        @Suppress("DEPRECATION")
+        Handler().postDelayed(
+            {
+                startActivity(Intent(this@SplashScreen, MainActivity::class.java))
+                finish()
+            },
+            3000
+        )
+        val typeface : Typeface = Typeface.createFromAsset(assets, "Alibaba_Sans_Heavy.ttf")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
